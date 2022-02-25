aacb8ce51e0e38b5a4b0349111121a6dbcc1dd5d
vicenzocanatto <93734804+vicenzocanatto@users.noreply.github.com>
2022-02-25 15:27:20 -0300

    Add files via upload

--- PRO-C13-Modelo-main/sketch.js
+++ PRO-C13-Modelo-main/sketch.js
@@ -1,4 +1,4 @@
-var bow ,arrow ,background;
+var bow , arrow,  background;
 var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
 var score = 0;
@@ -11,6 +11,9 @@ function preload(){
   pink_balloonImage = loadImage("pink_balloon0.png");
   blue_balloonImage = loadImage("blue_balloon0.png");
   red_balloonImage = loadImage("red_balloon0.png");
+  // red_balloonImage = loadImage("redballoon0.png");
+  // red_balloonImage = loadImage("red_balloon0");
+  //red_balloonImage = loadImage("red_balloon0.png");
 
 }
 
@@ -22,7 +25,7 @@ function setup() {
   //criando fundo
   scene = createSprite(0,0,400,400);
   scene.addImage(backgroundImage);
-  scene.scale = 2.5;
+  scene.scale = 2.5
   
   // criando arco para atirar flecha
   bow = createSprite(380,220,20,50);
@@ -33,6 +36,7 @@ function setup() {
 }
 
 function draw() {
+ background(0);
   // movendo chão
     scene.velocityX = -3 
 
@@ -49,6 +53,7 @@ function draw() {
   }
   
   //criando inimigos continuamente
+ 
   var select_balloon = Math.round(random(1,4));
   
   if (World.frameCount % 100 == 0) {
@@ -62,50 +67,78 @@ function draw() {
       pinkBalloon();
     }
   }
-  drawSprites();
   text("Pontuação: "+ score, 300,50)
+  drawSprites();
 }
 
 
 // Criando flechas para arco
  function createArrow() {
-  var arrow= createSprite(100, 100, 60, 10);
+  var arrow = createSprite(100, 100, 60, 10);
   arrow.addImage(arrowImage);
   arrow.x = 360;
-  arrow.y=bow.y;
+  arrow.y = bow.y;
   arrow.velocityX = -4;
   arrow.lifetime = 100;
   arrow.scale = 0.3;
 }
 
- function redBalloon() {
-   var red = createSprite(0, Math.round(random(20, 370)), 10, 10);
+function redBalloon() {
+   var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
    red.addImage(red_balloonImage);
    red.velocityX = 3;
    red.lifetime = 150;
    red.scale = 0.1;
-}
+   if(red.isTouching(arrow)) {
+    score = score + 200;
+  }
+ }
 
 function blueBalloon() {
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(blue_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 0.1;
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
+  if(blue.isTouching(arrow)) {
+    score = score + 150;
+  }
 }
 
 function greenBalloon() {
   var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(green_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 0.1;
+  green.addImage(green_balloonImage);
+  green.velocityX = 3;
+  green.lifetime = 150;
+  green.scale = 0.1;
+  if(green.isTouching(arrow)) {
+    score = score + 100;
+  }
 }
 
 function pinkBalloon() {
-  var pink = createSprite(0,Math.round(random(20, 370)), 400, 200);
-  red.addImage(pink_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 1;
+  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  pink.addImage(pink_balloonImage);
+  pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 1;
+  if(pink.isTouching(arrow)) {
+    score = score + 50;
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
